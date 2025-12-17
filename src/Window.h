32c5d7891bc1fd3463f623ba04362483c32ab836
ChrisCPNV <chris.brandt@eduvaud.ch>
2025-12-17 12:45:41 +0100

    Refactor Window class to properly initialize SDL_Window and manage its destruction

--- src/Window.h
+++ src/Window.h
@@ -1,12 +1,29 @@
 #pragma once
 #include <SDL3/SDL.h>
+#include <iostream>
 
 class Window {
 public:
     Window(){
-        SDL_CreateWindow(
+        SDLWindow = SDL_CreateWindow(
             "Hello Window",
-            700, 300, 0
+            700, 300,
+            SDL_WINDOW_RESIZABLE |
+            SDL_WINDOW_MINIMIZED
         );
     }
+
+    Window(const Window&) = delete;
+    Window& operator=(const Window&) = delete;
+
+    ~Window() {
+        if (SDLWindow && SDL_WasInit(SDL_INIT_VIDEO)) {
+            SDL_DestroyWindow(SDLWindow);
+        } else {
+            std::cout << "Skipping SDL_DestroyWindow\n";
+        }
+    }
+
+private:
+    SDL_Window* SDLWindow{nullptr};
 };
