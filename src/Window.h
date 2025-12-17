3ec1854c91e53a0fde84caea59b9c0daf4a52b16
ChrisCPNV <chris.brandt@eduvaud.ch>
2025-12-17 13:17:06 +0100

    Refactor Window class to improve window creation and event handling in main application

--- src/Window.h
+++ src/Window.h
@@ -1,27 +1,38 @@
 #pragma once
 #include <SDL3/SDL.h>
-#include <iostream>
 
 class Window {
 public:
     Window(){
         SDLWindow = SDL_CreateWindow(
             "Hello Window",
-            700, 300,
-            SDL_WINDOW_RESIZABLE |
-            SDL_WINDOW_MINIMIZED
+            800, 300, 0
         );
+
+        const auto* Fmt{SDL_GetPixelFormatDetails(
+            GetSurface()->format
+        )};
+
+        SDL_FillSurfaceRect(
+            GetSurface(),
+            nullptr,
+            SDL_MapRGB(Fmt, nullptr, 50, 50, 50)
+        );
+
+        SDL_UpdateWindowSurface(SDLWindow);
+    }
+
+    SDL_Surface* GetSurface() const {
+        return SDL_GetWindowSurface(SDLWindow);
     }
 
     Window(const Window&) = delete;
     Window& operator=(const Window&) = delete;
 
     ~Window() {
-        if (SDLWindow && SDL_WasInit(SDL_INIT_VIDEO)) {
-            SDL_DestroyWindow(SDLWindow);
-        } else {
-            std::cout << "Skipping SDL_DestroyWindow\n";
-        }
+      if (SDLWindow && SDL_WasInit(SDL_INIT_VIDEO)) {
+        SDL_DestroyWindow(SDLWindow);
+      }
     }
 
 private:
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 #include "Window.h"
 
-int main(int argc, char** argv) {
+int main(int , char** ) {
     SDL_Init(SDL_INIT_VIDEO);
     Window GameWindow;
 
-    while(true) {
-        SDL_PumpEvents();
+    bool isRunning = true;
+    SDL_Event event;
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                isRunning = false;
+            }
+        }
     }
 
     SDL_Quit();
