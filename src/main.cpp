98e8748a4362d2d52a4cd95d5fda05b509db341f
ChrisCPNV <pl52dsy@eduvaud.ch>
2025-12-22 13:24:46 +0100

    Enhance event handling in main application and update CMake configuration for console output

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
 #include <iostream>
 #include <string>
 #include "Window.h"
 #include "ErrorHandling.h"
 
+void HandleEvent(SDL_Event& E) {
+    if (E.type == SDL_EVENT_QUIT) {
+        std::cout << "Quit event received.\n";
+    } else if (E.type == SDL_EVENT_KEY_DOWN) {
+        std::cout << "Key down pressed" << '\n';
+    } else if (E.type == SDL_EVENT_MOUSE_MOTION) {
+        std::cout << "Mouse moved" << '\n';
+    }
+}
+
 int main(int , char** ) {
+    std::cout << "Program started\n";
+    std::cout.flush();
+    
     SDL_Init(SDL_INIT_VIDEO);
     std::string GameName{"Roguelite"};
     Window GameWindow(GameName.c_str());
@@ -16,9 +28,14 @@ int main(int , char** ) {
     #endif
 
     bool isRunning = true;
-    SDL_Event event;
+    SDL_Event event; // Event variable to hold events
+
     while (isRunning) {
+        // Pump events to update the event queue
+        SDL_PumpEvents();
+
         while (SDL_PollEvent(&event)) {
+            HandleEvent(event);
             if (event.type == SDL_EVENT_QUIT) {
                 isRunning = false;
             }
